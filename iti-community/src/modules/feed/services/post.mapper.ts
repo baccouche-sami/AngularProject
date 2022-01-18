9ba7d6a26513351bb105a0aa534bffccc78f9cb3
NanonLevana <nanon.ouattara@levana.io>
2022-01-18 11:39:34 +0100

    post update

--- iti-community/src/modules/feed/services/post.mapper.ts
+++ iti-community/src/modules/feed/services/post.mapper.ts
@@ -9,38 +9,57 @@ export class PostMapper {
   }
 
   private parseMessage(message: string): PostMessage {
+    console.log('start parsing');
     // TODO rajouter png jpg et gif
-    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg)/gmi;
+    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg|png|gif)/gmi;
 
-     // TODO mp4,wmv,flv,avi,wav
-    const videoRegex = / /gmi;
+     // TODO mp4,wmv,flv,avi|wav,wav
+    const videoRegex = /\.(?:mp4|wmv|flv|avi|wav)/gmi;
 
      // TODO mp3,ogg,wav
-    const audioRegex = / /gmi;
+    const audioRegex = /\.(?:mp3|ogg|wav)/gmi;
 
     const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
     const attachements: MessageElement[] = [];
 
     const pictureMatche = pictureRegex.exec(message);
     if (pictureMatche) {
+      console.log('match picture');
      // TODO ajouter un attachement de type image dans attachements
+     attachements.push({
+       type: 'image',
+       url: message
+     } as MessageImageElement);
     }
 
     const videoMatche = videoRegex.exec(message)
     if (videoMatche) {
+      console.log('match video');
      // TODO ajouter un attachement de type video dans attachements
-
+     attachements.push({
+        type: 'video',
+        url: message
+      } as MessageVideoElement);
     }
 
     const audioMatche = audioRegex.exec(message)
     if (audioMatche) {
+      console.log('match audio');
      // TODO ajouter un attachement de type audio dans attachements
-
+      attachements.push({
+        type: 'audio',
+        url: message
+      } as MessageAudioElement);
     }
 
     const youtubeMatche = youtubeRegex.exec(message)
     if (youtubeMatche) {
+      console.log('match youtube');
      // TODO ajouter un attachement de type youtube dans attachements
+      attachements.push({
+        type: 'youtube',
+        videoId: message
+      } as MessageYoutubeElement);
     }
 
     return {
