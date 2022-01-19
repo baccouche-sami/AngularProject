98d9cacb811643d95148d34496d7ce529faeaa18
NanonLevana <nanon.ouattara@levana.io>
2022-01-19 17:58:18 +0100

    end

--- iti-community/src/modules/feed/component/post/post.component.ts
+++ iti-community/src/modules/feed/component/post/post.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import { DateTime } from 'luxon';
 import { element } from 'protractor';
+import { getReturnOfExpression } from 'utility-types';
 import { Post } from '../../post.model';
 import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.less']
+  styleUrls: ['./post.component.less'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class PostComponent implements OnInit, AfterViewInit {
   @Input()
@@ -17,7 +20,6 @@ export class PostComponent implements OnInit, AfterViewInit {
   anchor: ElementRef<HTMLDivElement>;
 
   linkList : String[];
-  messageArray : String[];
 
   constructor(
     private postService: PostService
@@ -41,23 +43,27 @@ export class PostComponent implements OnInit, AfterViewInit {
     const fileRegex = /http[s]?:\/\/\S+?\.(?:jpeg|jpg|png|gif|mp4|wmv|flv|avi|wav|mp3|ogg|wav)/gmi;
 
     const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
-
-    const usernameRegex = /@[A-Za-z0-9_]\w+/gmi;
     
+    const usernameRegex = /@[A-Za-z0-9_]\w+/gmi
+
 
     let youtubeList = this.post.message.text.content.match(youtubeRegex);
     let fileList = this.post.message.text.content.match(fileRegex);
+    let userNameList = this.post.message.text.content.match(usernameRegex);
     
     this.post.message.text.content = this.post.message.text.content.replace(fileRegex, '');
     this.post.message.text.content = this.post.message.text.content.replace(youtubeRegex, '');
+    console.log(userNameList);
+    
+    userNameList?.forEach(element => {
+      let replacement = " <b class='post-text-username'> "+ element +" </b> "
+      console.log(element);
+      
+      this.post.message.text.content = this.post.message.text.content.replace(element, replacement)
+    });
 
     this.linkList = [...youtubeList??[], ...fileList??[]];
 
     return null;
-  }
-
-  findUsername() {
-    const usernameRegex = /@[A-Za-z0-9_]\w+/gmi;
-    this.messageArray = this.post.message.text.content.split(usernameRegex);
   }
 }
